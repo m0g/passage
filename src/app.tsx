fd4d58895d192b0d20667c45d181bf42da79aadf
Loïc Nogues <nogues.loic@gmail.com>
2017-12-29 16:46:36 +0100

    Generating a QR code of the pub key

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-var keypair = require('keypair');
- 
-var pair = keypair();
-console.log(pair);
-export default class App extends React.Component {
+import keypair from 'keypair';
+import QRCode from 'react-native-qrcode';
+
+interface State {
+  pair: { public: string, private: string };
+}
+
+export default class App extends React.Component<State> {
+  public state: State;
+
+  constructor(props) {
+    super(props);
+
+    const pair = keypair();
+    console.log(pair);
+    this.state = { pair: pair };
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Welcome to Passage.</Text>
-        <Text>{pair.public}</Text>
-        <Text>{pair.private}</Text>
+        <QRCode
+          value={this.state.pair.public}
+          size={200}
+          bgColor='purple'
+          fgColor='white'/>
+        <Text>{this.state.pair.public}</Text>
       </View>
     );
   }
