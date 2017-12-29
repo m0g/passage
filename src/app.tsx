0b0d5a3e24dd53113eec3d88ae6731067fcc9295
Loïc Nogues <nogues.loic@gmail.com>
2017-12-29 16:28:49 +0100

    generating basic key pair

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+var keypair = require('keypair');
+ 
+var pair = keypair();
+console.log(pair);
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Welcome to Passage.</Text>
+        <Text>{pair.public}</Text>
+        <Text>{pair.private}</Text>
       </View>
     );
   }
